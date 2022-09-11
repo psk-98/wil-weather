ecc8e2f1d825d4767da31a3526ea382fc8467028
psk-98 <paulk198@hotmail.com>
2022-09-11 22:08:28 +0200

    fix key

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ ReactDOM.render(
       <Home />
     </div>
     <Footer />
+    {process.env.REACT_APP_OPEN_WEATHER_API_KEY}
     <div className="circle1"></div>
     <div className="circle2"></div>
   </Provider>,
